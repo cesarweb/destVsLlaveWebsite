de220004c3885a7e5b0e24b4ffebd9ffb74e58e7
cesarweb <cmontiel@arca.gob.ar>
2022-10-12 09:23:55 -0300

    fixed urls.py

--- Destornilladores_Vs_Llaves/urls.py
+++ Destornilladores_Vs_Llaves/urls.py
@@ -13,29 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from django.views.generic import TemplateView
-from . import views as sysViews
 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-"""Destornilladores_Vs_Llaves URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
@@ -47,8 +25,4 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='index'),
     path("update_server/", sysViews.update, name="update"),
     path('test', TemplateView.as_view(template_name='test.html'), name='test'),
-]
-
-
-
 ]
